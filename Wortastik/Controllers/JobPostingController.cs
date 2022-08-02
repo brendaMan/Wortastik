3976b02542058f329ff7a9ff372529bdbf36f251
ARCHE-HOME\manrique <manrique@arche-it.de>
2022-08-02 11:25:37 +0200

    Add project files.

---
+++ Wortastik/Controllers/JobPostingController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Worktastic.Models;
+
+namespace Wortastik.Controllers
+{
+    public class JobPostingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateEditJobPosting(int id)
+        {
+            return View();
+        }
+        public IActionResult CreateEditJob(JobPosting jobPosting)
+        {
+            // TODO
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Wortastik/Data/Migrations/20220608071754_addedJobPostingTable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Wortastik.Data.Migrations
+{
+    public partial class addedJobPostingTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "JobPosting",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Jobtitle = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    JobLocation = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Salary = table.Column<int>(type: "int", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CompanyName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ContactPhone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ContactMail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ContactWebsite = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CompanyImage = table.Column<byte[]>(type: "varbinary(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobPosting", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JobPosting");
+        }
+    }
+}
